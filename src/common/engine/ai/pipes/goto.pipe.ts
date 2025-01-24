722e43ee56f4d6bc850fb0ff20a21250cc2427d1
sliw39 <sliw39@gmail.com>
2025-01-24 08:57:06 +0100

    Integration Astar <=> gotoPipe

--- src/common/engine/ai/pipes/goto.pipe.ts
+++ src/common/engine/ai/pipes/goto.pipe.ts
@@ -1,8 +1,11 @@
+import { AstarGrid } from "@engine/pathfinder.engine";
 import { AiPerception, Behavior, GenericPipe } from "@engine/state-ai.engine";
-import { manhattanDistance } from "@utils/vectors.util";
+import { sleep } from "@utils/time.util";
+import { globalDirection, manhattanDistance } from "@utils/vectors.util";
 import { vec, Vector } from "excalibur";
 
 export class GotoPipe extends GenericPipe {
+  private _interrupted = false;
   constructor(
     private _targetProvider: () => Vector | null,
     behavior: Behavior
@@ -14,9 +17,30 @@ export class GotoPipe extends GenericPipe {
     return 1;
   }
   execute(ai: AiPerception): Promise<void> {
-    throw new Error("Method not implemented.");
+    const target = this._targetProvider();
+    if (!target) {
+      return sleep(1000);
+    }
+
+    const path = new AstarGrid(ai.player.pos, target).findPath();
+    let current = path.shift();
+
+    return new Promise((resolve) => {
+      const interval = setInterval(() => {
+        if (!current || this._interrupted) {
+          ai.player.move("stop");
+          clearInterval(interval);
+          resolve();
+          return;
+        }
+        ai.player.move(globalDirection(ai.player.pos, current));
+        if (ai.player.pos.distance(current) < 16) {
+          current = path.shift();
+        }
+      }, 100);
+    });
   }
   interrupt(): void {
-    throw new Error("Method not implemented.");
+    this._interrupted = true;
   }
 }
